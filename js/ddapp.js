50470dce34e48cb88b4fd7579e7902208395d513
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-06-29 14:37:43 -0400

    image swap function written

--- js/ddapp.js
+++ js/ddapp.js
@@ -1,3 +1,6 @@
+//anonymous function module keeps your script private.
+(() => {
+
 	// Identify your nodes
 	const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
 				dropZoneContainer = document.querySelector(".puzzle-board"),
@@ -9,7 +12,7 @@
 		console.log('dragging ' + this.dataset.piecenumber);
 
 	};
-	function placeDrag(event) {
+	function dropped(event) {
 		event.preventDefault(); //override default behaviour
 		console.log('whoops, dropped it!');
 		console.log(event.target.id);
@@ -20,13 +23,27 @@
 		console.log('Look at you go, dragging things around');
 
 	};
+	function imageflip() {
+		//Click thumbnail image to switch out puzzle pieces
+		//update the background image of the drop zone container
+			//let imageindex = this.dataset.imageref;
+			//newimagepath = `url(images/dd/backGround${imageindex}.jpg)`;
+
+		dropZoneContainer.style.backgroundImage = `url(images/dd/backGround${this.dataset.imageref}.jpg)`;
+		debugger;
+	}
 
 	// event handling at the bottom
-dragImages.forEach(piece => {
-	piece.addEventListener('dragstart', startDrag, false);
-});
-//the pieces look for being clicked/dragged, but the dropzones are looking for the drop.
-dropZones.forEach(zone => {
-	zone.addEventListener('drop', placeDrag, false);
-	zone.addEventListener('dragover', draggedOver, false);
-});
+	dragImages.forEach(piece => {
+		piece.addEventListener('dragstart', startDrag, false);
+	});
+	//the pieces look for being clicked/dragged, but the dropzones are looking for the drop.
+	dropZones.forEach(zone => {
+		zone.addEventListener('drop', dropped, false);
+		zone.addEventListener('dragover', draggedOver, false);
+	});
+
+	puzzleSelectors.forEach(button => {
+		button.addEventListener('click', imageflip, false);
+	});
+})();
