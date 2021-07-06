5a1668a554533fcc852a5af3d1cb11de4b75a4b7
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-07-06 13:39:11 -0400

    added drop function, moving images, and fixing bugs

--- js/ddapp.js
+++ js/ddapp.js
@@ -3,47 +3,53 @@
 
 	// Identify your nodes
 	const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
-				dropZoneContainer = document.querySelector(".puzzle-board"),
+				dropContainer = document.querySelector(".puzzle-board"),
 				dragImages = document.querySelectorAll(".puzzle-image"),
 				dropZones = document.querySelectorAll(".drop-zone");
 
 	// functions go in the middle
-	function startDrag() {
-		console.log('dragging ' + this.dataset.piecenumber);
-
-	};
-	function dropped(event) {
-		event.preventDefault(); //override default behaviour
-		console.log('whoops, dropped it!');
-		console.log(event.target.id);
-
-	};
+	function startDrag(event) {
+		console.log('dragging ' + this.id);
+		// Saves a reference to the element that is being dragged.
+		// Then we can retrieve it later to place it.
+		event.dataTransfer.setData("dragTarget", this.id);
+	}
 	function draggedOver(event) {
 		event.preventDefault(); //override default behaviour
 		console.log('Look at you go, dragging things around');
+	}
 
-	};
+	function dropped(event) {
+		event.preventDefault(); //override default behaviour, allow the drop
+
+		//if there's already an image in the container, return statement
+		//return statement ends the function
+		if (this.children.length > 0) { return; }
+
+		let targetImage = document.querySelector(`#${event.dataTransfer.getData('dragTarget')}`);
+		//retrieve whatever element was grabbed
+		//event.dataTransfer.getData("dragTarget");
+		this.appendChild(targetImage);
+	}
 	function imageflip() {
 		//Click thumbnail image to switch out puzzle pieces
 		//update the background image of the drop zone container
 			//let imageindex = this.dataset.imageref;
 			//newimagepath = `url(images/dd/backGround${imageindex}.jpg)`;
-
-		dropZoneContainer.style.backgroundImage = `url(images/dd/backGround${this.dataset.imageref}.jpg)`;
-		debugger;
+		dropContainer.style.backgroundImage = `url(images/dd/backGround${this.dataset.imageref}.jpg)`;
 	}
 
-	// event handling at the bottom
+// event handling at the bottom
 	dragImages.forEach(piece => {
-		piece.addEventListener('dragstart', startDrag, false);
+		piece.addEventListener('dragstart', startDrag);
 	});
 	//the pieces look for being clicked/dragged, but the dropzones are looking for the drop.
 	dropZones.forEach(zone => {
-		zone.addEventListener('drop', dropped, false);
-		zone.addEventListener('dragover', draggedOver, false);
+		zone.addEventListener('drop', dropped);
+		zone.addEventListener('dragover', draggedOver);
 	});
 
 	puzzleSelectors.forEach(button => {
-		button.addEventListener('click', imageflip, false);
+		button.addEventListener('click', imageflip);
 	});
 })();
